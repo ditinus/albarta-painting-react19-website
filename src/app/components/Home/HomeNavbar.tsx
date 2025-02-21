e0d41df504794e12bed31c5f5a4e31b5220f359f
vikrant-ditinus <vikrant.intern@ditinustechnology.com>
2025-02-21 19:18:50 +0530

    Created the Blog component UI.

--- src/app/components/Home/HomeNavbar.tsx
+++ src/app/components/Home/HomeNavbar.tsx
@@ -1,8 +1,11 @@
-import { Navbar, Nav, Container, Row, Col, Image, NavDropdown } from 'react-bootstrap';
+'use client'
+import { Navbar, Nav, Container, Row, Col,  NavDropdown } from 'react-bootstrap';
 import './homeNavbar.css'
 import logoIcon from '../../../../public/assets/alberta-colour-painting-logo 1 (1).png';
 import phoneIcon from '../../../../public/assets/header-phone-icon.svg'
-import { Link } from 'react-router-dom'; // React Router for navigation
+import Link from 'next/link';
+import Image from 'next/image';
+
 
 const HomeNavbar = () => {
   return (
@@ -17,12 +20,12 @@ const HomeNavbar = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" className="toggle-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ml-auto" style={{ backgroundColor: "#0D378D1F", borderRadius: "30px", height: "60px", paddingTop: "9px" }}>
-                <Nav.Link as={Link} to="/#" className='mx-5'>Home</Nav.Link>
-                <Nav.Link as={Link} to="/#" className='mx-5'>About</Nav.Link>
-                <Nav.Link as={Link} to="/#" className='mx-5'>Services</Nav.Link>
-                <Nav.Link as={Link} to="/#" className='mx-5'>Gallery</Nav.Link>
-                <Nav.Link as={Link} to="/#" className='mx-5'>Blog</Nav.Link>
-                <Nav.Link as={Link} to="/#" className='mx-5'>Contact</Nav.Link>
+                <Nav.Link as={Link} href="/#" className='mx-5'>Home</Nav.Link>
+                <Nav.Link as={Link} href="/#" className='mx-5'>About</Nav.Link>
+                <Nav.Link as={Link} href="/#" className='mx-5'>Services</Nav.Link>
+                <Nav.Link as={Link} href="/#" className='mx-5'>Gallery</Nav.Link>
+                <Nav.Link as={Link} href="/#" className='mx-5'>Blog</Nav.Link>
+                <Nav.Link as={Link} href="/#" className='mx-5'>Contact</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Col>
---
+++ src/app/lib/types.ts
@@ -0,0 +1,11 @@
+export interface BlogPost {
+    id: string
+    title: string
+    slug: string
+    date: string
+    image: string
+    excerpt: string
+    content: string
+  }
+  
+  
