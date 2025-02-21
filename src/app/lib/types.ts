6a5db74b710d7af67a4075fffe6668e980c66f17
Arash Z <arash_75_z@yahoo.com>
2025-02-21 15:14:13 -0700

    add an contact us form

--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -8,4 +8,24 @@ export interface BlogPost {
     content: string
   }
   
-  
+export interface FormData {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+}
+
+export interface Errors {
+  name?: string;
+  email?: string;
+  phone?: string;
+  message?: string;
+}
+
+export interface ContactFormFieldsProps {
+  formData: FormData;
+  handleChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  errors: Errors;
+}
