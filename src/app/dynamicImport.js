8ec96db62ba61fd26d55d68eca26eae3d20abef3
Simarpreet-Ditinus <168518406+Simarpreet-Ditinus@users.noreply.github.com>
2025-03-05 13:07:52 +0530

    Update
    Clear main branch

--- src/app/dynamicImport.js
+++
@@ -1,6 +0,0 @@
-// lib/dynamicImport.js
-import dynamic from 'next/dynamic';
-
-// This function will dynamically import any component with SSR turned off
-export const dynamicImport = (component) =>
-  dynamic(() => import(component), { ssr: false });
