9d4fbee1cc9854df0328f1ca25b76c196374a692
vikrant-ditinus <156053067+vikrant-ditinus@users.noreply.github.com>
2025-03-05 14:33:21 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
