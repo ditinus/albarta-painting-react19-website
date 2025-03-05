b1f1c9dea20c9d91d87db722740a20b1d2f6d757
Veena <veena.sharma@ditinustechnology.com>
2025-03-05 18:37:02 +0530

    changed contact section

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,19 +1,28 @@
 "use client";
 
 import ContactFormSection from "@/components/ContactFormSection";
+import ContactGalleryHero from "@/components/ContactGalleryHero";
 import Hero from "@/components/hero";
 import HomeBanner from "@/components/HomeBanner/home-banner";
 import { Navbar } from "@/components/navbar";
 import OurGallery from "@/components/OurGallery";
 import TrustBadges from "@/components/trust-badges";
 import Footer from "@/components/ui/footer";
+import blogsBanner from "@/public/blogs-banner.png";
 
 export default function gallery() {
   return (
     
     <main className="flex min-h-screen flex-col">
-      <Navbar/>
-      <Hero/>
+      <ContactGalleryHero
+  title="OUR "
+  title2="GALLERY"
+  subtitle="Quality Craftsmanship | Beautiful Finishes | Happy Clients"
+  imageSrc={blogsBanner}
+  quoteLink="/quote"
+  bookLink="/book"
+  quoteText="Get a Free Quote"
+/>
       {/* <TrustBadges/> */}
  <OurGallery/>
       <HomeBanner/>
